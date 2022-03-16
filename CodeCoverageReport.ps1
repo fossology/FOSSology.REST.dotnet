d8ebbf5170845709bc63a1184afb9207d1a33d8f
Thomas Graf <tngraf@gmx.de>
2022-03-16 08:06:57 +0100

    test: add code coverage support

---
+++ CodeCoverageReport.ps1
@@ -0,0 +1,41 @@
+# -------------------------------------------------------
+# Create Code Covergae Report - Powershell based approach
+# -------------------------------------------------------
+
+$testfolder = "./.tests"
+
+# 1. Remove all TestResult folders and files
+Write-Host "Removing all 'TestResults' folders..."
+$testfolders = Get-ChildItem -Path . "TestResults" -Recurse -Directory
+foreach ($folder in $testfolders) {
+    Remove-Item $folder.FullName -Recurse
+}
+
+Write-Host "Removing all 'coverage.cobertura.xml' files..."
+$coberturaFiles = Get-ChildItem -Path . "coverage.cobertura.xml" -Recurse
+foreach ($file in $coberturaFiles) {
+    Remove-Item $file.FullName
+}
+
+# 2. Run tests
+Write-Host "Running unit tests..."
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
+
+# 3 Generate reports
+Write-Host "Generating reports..."
+$coberturaFiles = Get-ChildItem -Path . "coverage.cobertura.xml" -Recurse
+$list = ""
+for ($i=0; $i -lt $coberturaFiles.Count; $i++) {
+    $list = $list + $coberturaFiles[$i].FullName
+    $list = $list + ";"
+}
+
+$cmd = "-reports:" + $list
+$targetdir = "-targetdir:" + $testfolder + "\coverage-report"
+reportgenerator $cmd $targetdir -reporttypes:"Html"
+
+$targetdir = "-targetdir:" + $testfolder + "\badges"
+reportgenerator $cmd $targetdir -reporttypes:"Badges"
+
+$targetdir = "-targetdir:" + $testfolder
+reportgenerator $cmd $targetdir -reporttypes:"Cobertura;JsonSummary"
